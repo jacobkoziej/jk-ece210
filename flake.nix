f965e29739c01c28b03ba52a1490fdde06859658
Jacob Koziej <jacobkoziej@gmail.com>
2023-12-29 01:48:46 -0500

    Add `flake.nix`

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "jk-ece210";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        python = pkgs.python312;
+
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+	    pkgs.texliveFull
+            pkgs.nixpkgs-fmt
+            python
+          ];
+        };
+      }
+    );
+}
