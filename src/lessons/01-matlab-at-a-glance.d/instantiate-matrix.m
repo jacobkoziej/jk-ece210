a97c3b237cb734fb4fdcdb9b01980e1fed4756c3
Jacob Koziej <jacobkoziej@gmail.com>
2024-01-09 23:36:00 +0100

    Add `instantiate-matrix.m`

---
+++ src/lessons/01-matlab-at-a-glance.d/instantiate-matrix.m
@@ -0,0 +1,10 @@
+c = 13;            % create a scalar
+u = [5, 6, 7, 8];  % create a row vector
+v = [1; 2; 3; 4];  % create a column vector
+
+A = [              % create a matrix
+     1, 3
+     4, 2
+    ];
+
+B = [A, A];        % we can even compose matrices!
