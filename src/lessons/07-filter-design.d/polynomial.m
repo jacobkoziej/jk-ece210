a19e31275624d28fc3809d33c01be5d808204f08
Jacob Koziej <jacobkoziej@gmail.com>
2024-03-11 04:08:02 -0400

    Perform calculations before plotting

--- src/lessons/07-filter-design.d/polynomial.m
+++ src/lessons/07-filter-design.d/polynomial.m
@@ -2,6 +2,9 @@ l = 1e6;
 s = randn(1, l);
 S = fftshift(fft(s));
 
+s_filt = filter(b, a, s);
+S_filt = fftshift(fft(s_filt));
+
 figure;
 
 subplot(2, 1, 1);
@@ -10,9 +13,6 @@ title('Unfiltered');
 ylabel('|S| [dB]');
 xlim([-1, 1] * fn);
 xlabel('f [Hz]');
-
-s_filt = filter(b, a, s);
-S_filt = fftshift(fft(s_filt));
 
 subplot(2, 1, 2);
 plot(fs / l * (-l / 2:l / 2 - 1), 20 * log10(abs(S_filt)));
