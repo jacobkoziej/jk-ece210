1143929a12774c044a776cd11ad2a4cba39b5cec
Jacob Koziej <jacobkoziej@gmail.com>
2024-04-15 03:07:21 -0400

    Add `2-D DFT` section

--- src/lessons/10-image-compression.d/gen.m
+++ src/lessons/10-image-compression.d/gen.m
@@ -7,7 +7,20 @@ clc;
 clear;
 close all;
 
+out = @(fig, file) exportgraphics(fig, file, 'ContentType', 'vector');
+
 A = imread('original.jpg');
 B = rgb2gray(A);
 
 imwrite(B, 'grayscale.jpg');
+
+B_fft = fftshift(fft2(B));
+B_abs = abs(B_fft);
+
+f = figure;
+imshow( ...
+       log10(B_abs) ./ log10(max(B_abs, [], 'all')), ...
+       'Interpolation', ...
+       'bilinear');
+
+out(f, 'gray-fft2.eps');
