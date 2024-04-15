fe80690801a025690fef8ba70a0e32b18ff07c94
Jacob Koziej <jacobkoziej@gmail.com>
2024-04-15 03:20:52 -0400

    Add `Dropping Samples` section

--- src/lessons/10-image-compression.d/gen.m
+++ src/lessons/10-image-compression.d/gen.m
@@ -24,3 +24,13 @@ imshow( ...
        'bilinear');
 
 out(f, 'gray-fft2.eps');
+
+B_abs_sort = sort(B_abs(:));
+
+for drop = [0.0000, 0.9000, 0.9900, 0.9950, 0.9975, 0.9990]
+    threshold = B_abs_sort(floor(numel(B_abs_sort) * drop) + 1);
+    mask = B_abs >= threshold;
+
+    compressed = uint8(ifft2(ifftshift(B_fft .* mask)));
+    imwrite(compressed, sprintf('%1.4f.jpg', drop));
+end
